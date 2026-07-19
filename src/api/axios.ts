96f7c8ed658ff7813615c7df32052f8e487113f5
mohd nihal <mohdsahil3533@gmail.com>
2026-07-19 13:08:02 +0530

    test

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -8,7 +8,6 @@ const api = axios.create({
   },
 });
 
-// Add a request interceptor to automatically attach the token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
